bf5f2cd9026ee1cf0bdab57ae7e013981f70bccd
Julian Klappenbach <jklappenbach>
2012-10-22 11:47:18 -0500

    Converted test environment over to HTML5 libraries.

--- test/webapp/index.js
+++ test/webapp/index.js
@@ -5,7 +5,8 @@ requirejs.config({
         vcdiff: 'js/lib/vcdiff/vcdiff',
         jcookies: 'js/lib/jquery/jquery.cookies.amd',
         cajeta: 'js/lib/cajeta/cajeta',
-        cajetaHtml4: 'js/lib/cajeta/cajeta.html4',
+        cajetaHtml5: 'js/lib/cajeta/cajeta.html5',
+        cajetaSvg: 'js/lib/cajeta/cajeta.html5.svg',
         text: 'js/lib/require/plugin/text',
         order: 'js/lib/require/plugin/order',
         depend: 'js/lib/require/plugin/depend',
@@ -16,8 +17,8 @@ requirejs.config({
     }
 });
 
-define(['cajetaHtml4', 'homePage'], function(cajeta, homePage) {
-    cajeta.theApplication = new cajeta.Application();
-    cajeta.theApplication.addPage(homePage);
-    cajeta.theApplication.execute();
+define(['cajetaHtml5', 'homePage'], function(Cajeta, homePage) {
+    Cajeta.theApplication = new Cajeta.Application();
+    Cajeta.theApplication.addPage(homePage);
+    Cajeta.theApplication.execute();
 });
--- test/webapp/js/app/view/home/homePage.js
+++ test/webapp/js/app/view/home/homePage.js
@@ -1,21 +1,14 @@
 define([
     'jquery',
-    'cajetaHtml4',
-    'formExampleDiv',
-    'serverInteractionDiv',
+    'cajetaHtml5',
     'text!js/app/view/home/homePage.html'
-], function($, Cajeta, formExampleDiv, serverInteractionDiv, homePageText) {
+], function($, Cajeta, homePageText) {
     
-    // Create an alias for shortening namespace.
-    var Html4 = Cajeta.View.Html4;
+    // Create an alias for brief namespace.
+    var Html4 = Cajeta.View.Html5;
 
     var homePage = new Cajeta.View.Page({ componentId: Cajeta.View.homePage });
     homePage.setTemplate('homePage', homePageText);
-
-    var tabs = new Html4.TabList({ componentId: 'tabs', contentId: 'content' });
-    tabs.addChild({ title: 'Html4 Form Example', component: formExampleDiv });
-    tabs.addChild({ title: 'Server Interaction Example', component: serverInteractionDiv });
-    homePage.addChild(tabs);
 
     return homePage;
 });
